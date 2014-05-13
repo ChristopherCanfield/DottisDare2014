9f131ccc656b43c2efab380f57bfb0263cc06e5a
Christopher D Canfield <cdcanfield@gmail.com>
2014-05-13 07:25:24 -0400

    Added clue875 & clue925; set map to point to all clues

--- DottisDare/server/clues/clue925.php
+++ DottisDare/server/clues/clue925.php
@@ -1,4 +1,4 @@
-		<div class="modal fade" id="purpleCircle" tabindex="-1" role="dialog" aria-labelledby="Purple Circle" aria-hidden="true">
+		<div class="modal fade" id="blueCircle" tabindex="-1" role="dialog" aria-labelledby="Blue Circle" aria-hidden="true">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
@@ -6,11 +6,11 @@
 					</div>
 					
 					<div class="modal-body">
-						<p>Dotti became a leader of an intermediate troop.</p>	
+						<p>Dotti graduated from North Penn High School.</p>	
 					</div>
 					
-					<img src="images/clues/Birdhouse178.jpg" alt="Speed limit sign by dining hall" 
-							height="314" width="161" class="img-rounded" />
+					<img src="images/clues/EagleBridge925.jpg" alt="Birdhouse at Conestoga" 
+							height="201" width="314" class="img-rounded" />
 					
 					<div class="modal-footer">
 						<button type="button" class="btn btn-primary"></button>
