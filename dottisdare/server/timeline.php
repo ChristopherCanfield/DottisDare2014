f37d44951bd60eccebf7232c2be5719d3d82ee61
Christopher D. Canfield <cdcanfield@gmail.com>
2014-05-14 23:20:01 -0400

    Added clue text to timeline

--- dottisdare/server/timeline.php
+++ dottisdare/server/timeline.php
@@ -12,29 +12,41 @@
 						
 						<ul id="sortable">
 							<li id="clue173" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 1</li>
+								Clue 173<br>
+								Dotti was left stranded on a rock in the middle of a raging river while whitewater rafting with her Senior Troop.</li>
 							<li id="clue178" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 2</li>
+								Clue 178<br>
+								Dotti became a leader of an intermediate troop.</li>
 							<li id="clue217" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 3</li>
+								Clue 217<br>
+								Dotti began working for Girl Scouts.</li>
 							<li id="clue218" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 4</li>
+								Clue 218<br>
+								Dotti became her council's delegate for the Girl Scout National Convention. She would reprise this role many times.</li>
 							<li id="clue249" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 5</li>
+								Clue 249<br>
+								Dotti joined the Lansdale Ambulance Corp.</li>
 							<li id="clue267" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 6</li>
+								Clue 267<br>
+								Dotti moved to Chicago.</li>
 							<li id="clue472" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 7</li>
+								Clue 472<br>
+								Dotti worked as a cook during summer camp at Camp Laughing Waters.</li>
 							<li id="clue514" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 7</li>
+								Clue 514<br>
+								A member of Dotti's Senior Troop #553 was named the first Gold Award recipient in the Greater Philadelphia Council.</li>
 							<li id="clue542" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 7</li>
+								Clue 542<br>
+								Dotti began traveling to Appalachia each summer to help build and repair homes for the poor.</li>
 							<li id="clue874" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 7</li>
+								Clue 874<br>
+								Dotti was awarded the highest national Girl Scout honor - the Thanks Badge II.</li>
 							<li id="clue875" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 7</li>
+								Clue 875<br>
+								Dotti led a Pilot troop of five-year-olds to help gather information before implementation of the Daisy program.</li>
 							<li id="clue925" class="ui-state-default"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-								Item 7</li>
+								Clue 925<br>
+								Dotti graduated from North Penn High School.</li>
 						</ul>
 						
 						<ul>
