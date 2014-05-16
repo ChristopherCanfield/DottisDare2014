3b9306a652c1dc108c8f767df0cb8ced5735827f
Christopher D. Canfield <cdcanfield@gmail.com>
2014-05-15 23:48:12 -0400

    Updated many of the clue code subpages to include the new functionality

--- DottisDare/server/clues/clue514.php
+++ DottisDare/server/clues/clue514.php
@@ -1,3 +1,8 @@
+		<?php
+			$clueId = 514;
+			$clueFound = array_key_exists($clueId, $cluesFound);
+		?>
+		
 		<div class="modal fade" id="greenCircle" tabindex="-1" role="dialog" aria-labelledby="Green Circle" aria-hidden="true">
 			<div class="modal-dialog modal-dialog-clue">
 				<div class="modal-content">					
@@ -6,17 +11,57 @@
 							Clue Code
 						</h3>
 						
-						<form class="form-signin cluecode" role="form">
-							<img src="images/clues/514.jpg" alt="Ginder Barn" 
-									width="314" height="229" class="img-rounded modal-image-clue" />
+						<?php
+							$onSubmitValue = ""; 
+							if (!$clueFound)
+							{
+								// Change the value at the end for each clue.
+								$onSubmitValue = 'validateClue(\'' . $troop . '\', ' . $clueId . ', \'greenCircle\');';
+							}
+							else
+							{
+								// Change the value at the end for each clue.
+								$onSubmitValue = 'closeModalOpenTimeline(\'greenCircle\');';
+							}
 							
-					        <h4 class="form-signin-heading">
-					        	Enter the 3-digit code found on the back of your clue:
-					        </h4>
-					        
-					        <input type="text" class="form-control dottisdare" placeholder="" required autofocus>
-					        <button class="btn btn-lg btn-primary dottisdare" type="submit">Submit Clue Code</button>
-					        <button type="button" class="btn btn-lg dottisdare" data-dismiss="modal">Close</button>
+							echo
+							'<form class="form-signin cluecode" role="form" id="form' . $clueId . '" ' . 
+								'onsubmit="' . $onSubmitValue . 'return false;">' .
+							
+							// Update the alt text, width and height for each clue.
+							'<img src="images/clues/' . $clueId . '.jpg" alt="Ginder Barn" 
+								width="314" height="229" class="img-rounded modal-image-clue" />';
+							
+							$clueTextStyleVisibility = ($clueFound) ? 'style="visibility:visible"' : '';
+							echo
+							'<h4 id="clue' . $clueId . '" class="clue-success" ' . $clueTextStyleVisibility . '>' .
+								// The clue text.
+								'Test TEST' .
+							'</h4>';
+							
+							$submitButtonText = "";
+							if (!$clueFound)
+							{
+								echo
+								'<h4 id="submitText' . $clueId . '" class="form-signin-heading">
+					        		Enter the 3-digit code found on the back of your clue:
+					        	</h4>
+								<input type="text" id="input' . $clueId . '" class="form-control dottisdare clue" placeholder="" required autofocus>
+								<h4 id="incorrect' . $clueId . '" class="clue-invalid">Incorrect clue code.</h4>';
+								$submitButtonText = "Submit Clue Code";
+							}
+							else 
+							{
+								$submitButtonText = "Open Timeline";
+							}
+							
+							echo
+							'<button class="btn btn-lg btn-primary dottisdare" type="submit" id="submitButton' . $clueId . '">
+								' . $submitButtonText . '
+							</button>
+							<button type="button" class="btn btn-lg dottisdare" data-dismiss="modal">Close</button>';
+						?>
+
 					      </form>
 					</div>
 				</div>
