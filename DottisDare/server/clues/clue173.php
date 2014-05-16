defb6bd0f5dace1c60cdee06b7f75a254fca83b8
Christopher D. Canfield <cdcanfield@gmail.com>
2014-05-15 23:11:22 -0400

    Changed CLUE_ID to variable rather than constant

--- DottisDare/server/clues/clue173.php
+++ DottisDare/server/clues/clue173.php
@@ -1,6 +1,7 @@
 		<?php
-			define("CLUE_ID", 173);
-			$clueFound = array_key_exists(CLUE_ID, $cluesFound);
+			$clueId = 173;
+			$clueFound = array_key_exists($clueId, $cluesFound);
+			
 		?>
 		
 		<div class="modal fade" id="blueTriangle" tabindex="-1" role="dialog" aria-labelledby="Blue Triangle" aria-hidden="true">
@@ -15,7 +16,7 @@
 							$onSubmitValue = ""; 
 							if (!$clueFound)
 							{
-								$onSubmitValue = 'validateClue(\'' . $troop . '\', ' . CLUE_ID . ', \'blueTriangle\');';
+								$onSubmitValue = 'validateClue(\'' . $troop . '\', ' . $clueId . ', \'blueTriangle\');';
 							}
 							else
 							{
@@ -23,15 +24,15 @@
 							}
 							
 							echo
-							'<form class="form-signin cluecode" role="form" id="form' . CLUE_ID . '" ' . 
+							'<form class="form-signin cluecode" role="form" id="form' . $clueId . '" ' . 
 								'onsubmit="' . $onSubmitValue . 'return false;">' .
 						
-							'<img src="images/clues/' . CLUE_ID . '.jpg" alt="Pool House" 
+							'<img src="images/clues/' . $clueId . '.jpg" alt="Pool House" 
 								width="314" height="215" class="img-rounded modal-image-clue" />';
 							
 							$clueTextStyleVisibility = ($clueFound) ? 'style="visibility:visible"' : '';
 							echo
-							'<h4 id="clue' . CLUE_ID . '" class="clue-success" ' . $clueTextStyleVisibility . '>' .
+							'<h4 id="clue' . $clueId . '" class="clue-success" ' . $clueTextStyleVisibility . '>' .
 								// The clue text.
 								'Test TEST' .
 							'</h4>';
@@ -40,10 +41,10 @@
 							if (!$clueFound)
 							{
 								echo
-								'<h4 id="submitText' . CLUE_ID . '" class="form-signin-heading">
+								'<h4 id="submitText' . $clueId . '" class="form-signin-heading">
 					        		Enter the 3-digit code found on the back of your clue:
 					        	</h4>
-								<input type="text" id="input' . CLUE_ID . '" class="form-control dottisdare" placeholder="" required autofocus>';
+								<input type="text" id="input' . $clueId . '" class="form-control dottisdare" placeholder="" required autofocus>';
 								$submitButtonText = "Submit Clue Code";
 							}
 							else 
@@ -52,11 +53,11 @@
 							}
 							
 							echo
-							'<button class="btn btn-lg btn-primary dottisdare" type="submit" id="submitButton' . CLUE_ID . '">
+							'<button class="btn btn-lg btn-primary dottisdare" type="submit" id="submitButton' . $clueId . '">
 								' . $submitButtonText . '
 							</button>
 							<button type="button" class="btn btn-lg dottisdare" data-dismiss="modal">Close</button>
-							<h4 id="incorrect' . CLUE_ID . '" class="clue-invalid">Incorrect clue code.</h4>';
+							<h4 id="incorrect' . $clueId . '" class="clue-invalid">Incorrect clue code.</h4>';
 						?>
 
 					      </form>
